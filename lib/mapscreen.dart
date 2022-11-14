c55d57f0b159539c72414d1ba64e9c5dc81d33ae
Lucaslala88 <delfossesomain@gmail.com>
2022-11-14 16:44:18 +0100

    recovery

--- lib/mapscreen.dart
+++ lib/mapscreen.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
 
 class mapscreen extends StatelessWidget {
+  int _counter = 0;
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("MAP")),
       body: const Center(
         child: Text('Pokedex'),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        color: Colors.red,
+        child: Container(
+          height: 50,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              IconButton(
+                icon: const Icon(
+                  Icons.navigate_before,
+                  color: Colors.black,
+                ),
+                onPressed: _decrementCounter,
+              ),
+              IconButton(
+                icon: const Icon(
+                  Icons.navigate_next,
+                  color: Colors.white,
+                ),
+                onPressed: _incrementCounter,
+              ),
+            ],
+          ),
+        ),
       ),
       drawer: Drawer(
         child: ListView(
@@ -41,4 +80,6 @@ class mapscreen extends StatelessWidget {
       ),
     );
   }
+
+  void setState(Null Function() param0) {}
 }
