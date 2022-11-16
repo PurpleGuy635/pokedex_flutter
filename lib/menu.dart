b303ecbd2ea82206478f0c8f011eea73ab27f100
francois <admin@purpleguy.ovh>
2022-11-16 09:20:39 +0100

    push menu recup

--- lib/menu.dart
+++ lib/menu.dart
@@ -8,38 +8,68 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Pokedex - Menu"),
       ),
-      body: Center(
-        /*HomeScreen container(){
-        Positioned(
-            height: 50,
-            width: 50,
-            right: 100,  
-            top: 50,
-            Image.asset("./asset/Kanto.png"),
-              );
-        }*/
-
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                //image: AssetImage("./asset/dracaufeu.png"), fit: BoxFit.cover)),
+                image: AssetImage("./asset/images/alola.png"),
+                fit: BoxFit.cover)),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Padding(padding: EdgeInsets.only(bottom: 25)),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/route1');
-                },
-                child: const Text("Pokedex - MAP"),
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(350, 75),
-                )),
-            const Padding(padding: EdgeInsets.only(bottom: 10)),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/route2');
-              },
-              child: const Text("Pokedex"),
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(350, 75),
-              ),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Positioned(
+              child: (Image.asset(
+                "./asset/images/Logo.png",
+                height: 100,
+                width: 100,
+              )),
+            ),
+            Positioned(
+              top: 10,
+              left: 1000,
+              child: (Image.asset(
+                "./asset/images/Logo.png",
+                height: 100,
+                width: 100,
+              )),
+            ),
+            Column(
+              children: [
+                const Padding(padding: EdgeInsets.only(bottom: 25)),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/route1');
+                    },
+                    child: const Text("Pokedex - MAP"),
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(350, 75),
+                    )),
+                const Padding(padding: EdgeInsets.only(bottom: 10)),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/route2');
+                  },
+                  child: const Text("Pokedex"),
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(350, 75),
+                  ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Positioned(
+                      right: 100,
+                      top: 10,
+                      child: (Image.asset(
+                        "./asset/images/Logo.png",
+                        height: 100,
+                        width: 100,
+                      )),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ],
         ),
